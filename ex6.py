771418354747b075a60ff2b580dc2cffa4bd38fd
bl15343 <bl15343@gmail.com>
2019-07-06 19:50:45 -0400

    Exercise 6 with study drills

---
+++ ex6.py
@@ -0,0 +1,47 @@
+#Create a varible types_of_people and assign it the value of 10
+types_of_people = 10
+
+#Create a variable x, with a formatted string value
+x = f"There are {types_of_people} types of people"
+
+#Create a variable binary with a string value of binary
+binary = "binary"
+#Create a variable do_not with a string value of don't
+do_not = "don't"
+#Create a variable y, with a formatted string value
+y = f"Those who know {binary} and those who {do_not}"
+
+#Print the string assigned to x
+print(x)
+#Print the string assigned to y
+print(y)
+
+#Print a formatted string with string assigned to x
+print(f"I said: {x}")
+#Print a formatted string with string assigned to y
+#encased in single quotes
+print(f"I also said:'{y}'")
+
+#Create a boolean variable hilarious and set it to False
+hilarious = False
+#Create a variable to hold a string with an empty variable binding
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+#Print a formatted string, pass in the variable hilarious to bind
+#to the variable evaluation braces
+print(joke_evaluation.format(hilarious))
+
+#Create a variable w with a string value
+w = "This is the left side of..."
+#Create a variable e with a string value
+e = "a string with a ride side."
+
+
+#Study drill 4
+#The addition operator acts as a concatenation operation
+#for strings
+print(w + e)
+
+#Study Drills 2/3
+#I see 5 instances of strings being inserted into strings
+
