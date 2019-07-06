0b9feebe9af60a1ef77d0bb62a343a7304d3c001
bl15343 <bl15343@gmail.com>
2019-07-06 16:46:55 -0400

    Hello world exercise 1 with study drills

---
+++ ex1.py
@@ -0,0 +1,13 @@
+#print("Hello World!")
+#print("Hello Again")
+#print("I like typing this")
+#print("This is fun.")
+#print('Yay! Printing.')
+#print("I'd much rather you 'not'.")
+#print('I "said" do not touch this.')
+
+#Study Drills
+print('Another line to print')
+
+#Adding an octothorpe creates a comment
+#Only one line is printing because i commented the rest out
