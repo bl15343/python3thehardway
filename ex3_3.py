921645fcc9fd7c145b95f174c885fe548e48b8c9
bl15343 <bl15343@gmail.com>
2019-07-06 19:05:06 -0400

    Add study drill 3.3 -- calculate the surface area of a sphere and volume of a sphere

---
+++ ex3_3.py
@@ -0,0 +1,7 @@
+#Calculate the area of a sphere with a radius
+#of 2 units. Using 3.14 for PI
+print("The area of a sphere with radius 2 is: ",  4 * 3.14 * (2 ** 2) )
+
+#Calculate the volume of a sphere with a radis of 2 units
+#Using 3.14 for PI
+print("The volume of a sphere with radius 2 is: ", (4/3) * 3.14 * ( 2 ** 3) )
