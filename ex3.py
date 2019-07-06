d00fd38cb78a8a33d5efbf066ef52209c4e70e2f
bl15343 <bl15343@gmail.com>
2019-07-06 17:07:30 -0400

    Force floating point operations by adding decimal precision

--- ex3.py
+++ ex3.py
@@ -3,30 +3,30 @@ print("I will now count my chickens:")
 
 #Divides 30 by 6 and adds the quotient to 25 and prints the string
 #Hens 30.0
-print("Hens", 25 + 30 / 6)
+print("Hens", 25.0 + 30.0 / 6.0)
 
 #Multiplies 25 * 3 and takes the modulus of 4 of that number
 #and subtracts it from 100
 #100 - (75 % 4 )
-print("Roosters", 100 - 25 * 3 % 4)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
 
 print("Now I will count the eggs:")
 
 # 3 + 2 + 1 - 5 + (0) - (1/4) + 6
 # (6) - 5 + (0) - (1/4) + 6 == 6.75
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
 
 print("Is it true that 3 + 2 < 5 - 7?")
 
 #Returns a boolean whether or not
 # 5 is less than -2
-print(3 + 2 < 5 - 7)
+print(3.0 + 2.0 < 5.0 - 7.0)
 
 #Prints the string What is 3+2? and the result 5
-print("What is 3 + 2?", 3 + 2)
+print("What is 3 + 2?", 3.0 + 2.0)
 
 #Prints the string What is 5-7? and the result -2
-print("What is 5 - 7?", 5 - 7)
+print("What is 5 - 7?", 5.0 - 7.0)
 
 print("Oh, that's why it's False.")
 
@@ -34,12 +34,12 @@ print("How about some more.")
 
 #Prints the string Is it greater? and the boolean evaluation
 #of is 5 greater than -2
-print("Is it greater?", 5 > -2)
+print("Is it greater?", 5.0 > -2.0)
 
 #Prints the string Is it greater or equal? and the boolean evaluation
 #of is 5 greater than or equal to -2
-print("Is it greater or equal?", 5 >= -2)
+print("Is it greater or equal?", 5.0 >= -2.0)
 
 #Prints the string Is it less or equal? and the boolean evalulation
 #of is 5 less than or equal to -2
-print("Is it less or equal?", 5 <= -2)
+print("Is it less or equal?", 5.0 <= -2.0)
